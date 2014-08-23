83ed499d4f92e2ca0b4bd1816629175e1a27fda5
Jing Jin <jing@konsu.lt>
2014-08-22 21:22:49 -0700

    Initial simple todo app with add and remove.

---
+++ src/com/flukiness/simpletodoapp/TodoActivity.java
@@ -0,0 +1,99 @@
+package com.flukiness.simpletodoapp;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.*;
+
+
+public class TodoActivity extends Activity {
+	ArrayList<String> items;
+	ArrayAdapter<String> itemsAdapter;
+	ListView lvItems;
+	EditText etNewItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        etNewItem = (EditText) findViewById(R.id.etNewItem);
+        
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        
+        setupListViewListener();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.todo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void addTodoItem(View v) {
+    	itemsAdapter.add(etNewItem.getText().toString());
+    	etNewItem.setText("");
+    	saveItems();
+    }
+    
+    private void setupListViewListener() {
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+    		@Override
+    		public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long rowId) {
+    			items.remove(position);
+    			itemsAdapter.notifyDataSetChanged();
+    			return true;
+    		}
+    	});
+    }
+    
+    private void readItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try {
+    		items = new ArrayList<String>(FileUtils.readLines(todoFile));
+    	} catch (IOException e) {
+    		items = new ArrayList<String>();
+    		e.printStackTrace();
+    	}
+
+    }
+    
+    private void saveItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try {
+    		FileUtils.writeLines(todoFile,  items);
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+}
